0c120b5a854d999ed058649b80daeaf7bf27e2c4
andergato <145510193+andergato@users.noreply.github.com>
2025-04-29 14:39:57 -0500

    Added starter code for when JSON pet classes are created

--- src/main/java/edu/mu/maven/AdoptMeGroup23/App.java
+++ src/main/java/edu/mu/maven/AdoptMeGroup23/App.java
@@ -1,10 +1,32 @@
 package edu.mu.maven.AdoptMeGroup23;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.google.gson.Gson;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+    	
         System.out.println("Hello World!");
+        
+        String directory = System.getProperty("user.dir");
+		String filePath =  directory  + File.separator + "AdoptMe_CS3330" + File.separator + "src" + File.separator + "main"
+				+ File.separator + "java" + File.separator + "resources" + File.separator
+				+ "pets.json";
+        Gson gson = new Gson();
+        
+        try {
+			FileReader fr = new FileReader(filePath);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+
+       
     }
 }
