ab13aa317708511e731a74c74e19c37814718ccd
andergato <145510193+andergato@users.noreply.github.com>
2025-05-01 10:04:26 -0500

    Created Simple GUI View

---
+++ src/main/java/edu/mu/maven/view/GUIView.java
@@ -0,0 +1,94 @@
+package edu.mu.maven.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+
+public class GUIView extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIView frame = new GUIView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUIView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1008, 713);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextPane txtpnPets = new JTextPane();
+		txtpnPets.setEditable(false);
+		txtpnPets.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		txtpnPets.setText("Pets!");
+		txtpnPets.setBounds(465, 10, 132, 76);
+		contentPane.add(txtpnPets);
+		
+		table = new JTable();
+		table.setBounds(313, 255, 484, 376);
+		contentPane.add(table);
+		
+		JButton btnAdopt = new JButton("Adopt");
+		btnAdopt.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnAdopt.setBounds(245, 151, 142, 55);
+		contentPane.add(btnAdopt);
+		
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnRemove.setBounds(439, 151, 142, 55);
+		contentPane.add(btnRemove);
+		
+		JButton btnView = new JButton("View Details");
+		btnView.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnView.setBounds(642, 151, 142, 55);
+		contentPane.add(btnView);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnSave.setBounds(823, 151, 142, 55);
+		contentPane.add(btnSave);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAdd.setBounds(64, 151, 142, 55);
+		contentPane.add(btnAdd);
+		
+		JComboBox sortBox = new JComboBox();
+		sortBox.setBounds(206, 264, 97, 21);
+		contentPane.add(sortBox);
+	}
+}
