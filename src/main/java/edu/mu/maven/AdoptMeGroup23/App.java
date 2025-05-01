98a8dc30acb4a529d8165e8f4e4f6f3a54259331
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-30 21:11:10 -0500

    working on json importation, having trouble with working around abstract class

--- src/main/java/edu/mu/maven/AdoptMeGroup23/App.java
+++ src/main/java/edu/mu/maven/AdoptMeGroup23/App.java
@@ -1,32 +1,40 @@
 package edu.mu.maven.AdoptMeGroup23;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
 import com.google.gson.Gson;
+import edu.mu.maven.Model.Pet;
+import com.google.gson.reflect.TypeToken;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Type;
+import java.io.IOException;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-    	
-        System.out.println("Hello World!");
-        
-        String directory = System.getProperty("user.dir");
-		String filePath =  directory  + File.separator + "AdoptMe_CS3330" + File.separator + "src" + File.separator + "main"
+    
+    List<Pet> pet = new ArrayList<>();
+    Gson gson = new Gson();
+      
+	try {
+		String directory = System.getProperty("user.dir");
+		String filePath =  directory  + File.separator + "src" + File.separator + "main"
 				+ File.separator + "java" + File.separator + "resources" + File.separator
 				+ "pets.json";
-        Gson gson = new Gson();
-        
-        try {
-			FileReader fr = new FileReader(filePath);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-
-       
+		BufferedReader br = new BufferedReader(new FileReader(filePath));
+	   Type type = new TypeToken<ArrayList<Pet>>() {}.getType();
+	   pet = gson.fromJson(br, type);
+	   System.out.println(pet);
+	   System.out.println(pet.get(0).GetName());
+	   br.close();
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
     }
 }
