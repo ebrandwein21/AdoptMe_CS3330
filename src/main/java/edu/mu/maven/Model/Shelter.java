47fd7938020a029fdfea839cbc6de33438a27e7d
andergato <145510193+andergato@users.noreply.github.com>
2025-05-03 12:03:48 -0500

    Started on comparitors and shelter classes

--- src/main/java/edu/mu/maven/Model/Shelter.java
+++ src/main/java/edu/mu/maven/Model/Shelter.java
@@ -1,8 +1,15 @@
 package edu.mu.maven.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shelter<T extends Pet>{
 	
 	//logic for storing pets
+	private List<T> petList = new ArrayList<>();
 	
-
+	public List<T> getPets(){
+		
+		
+	}
 }
