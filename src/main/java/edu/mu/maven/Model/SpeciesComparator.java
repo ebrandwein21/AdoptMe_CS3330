b03b6f5757d34a9dffc4b15f7468c19b1a0a027c
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-05-03 15:46:58 -0500

    fixed age comparator

--- src/main/java/edu/mu/maven/Model/SpeciesComparator.java
+++ src/main/java/edu/mu/maven/Model/SpeciesComparator.java
@@ -1,5 +1,11 @@
 package edu.mu.maven.Model;
 
-public interface SpeciesComparator {
+import java.util.Comparator;
 
+public class SpeciesComparator implements Comparator<Pet>
+{
+	public int compare(Pet pet1, Pet pet2)
+	{
+		return Integer.compare(pet1.GetAge(), pet2.GetAge());
+	}
 }
